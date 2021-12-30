58df81b8d926e59b0bf661b7b851984bbd643e6c
Rafał Zubala <rzubala@operatus.org>
2021-12-30 21:33:06 +0100

    initial

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,4 @@
-import * as functions from "firebase-functions";
+
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -7,3 +7,18 @@ import * as functions from "firebase-functions";
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+import * as functions from "firebase-functions";
+//import * as admin from 'firebase-admin'
+import * as express from 'express'
+import * as cors from 'cors'
+
+const app = express();
+app.use(cors({ origin: true }));
+
+app.get('/hello-world', (req, res) => {
+  return res.status(200).send('Hello World!');
+});
+
+exports.app = functions.https.onRequest(app);
+
