50c9ae5edb0164529789596e6ec1f7c46e7acf7c
Rafał Zubala <rzubala@operatus.org>
2021-12-31 12:16:13 +0100

    crud operations

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,3 @@
-
-
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
@@ -9,16 +7,108 @@
 // });
 
 import * as functions from "firebase-functions";
-//import * as admin from 'firebase-admin'
-import * as express from 'express'
-import * as cors from 'cors'
+import * as admin from "firebase-admin";
+import express from "express";
+import cors from "cors";
+import * as serviceAccount from "../permissions.json";
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+  databaseURL: "https://fir-api-2fa3d..firebaseio.com",
+});
+const db = admin.firestore();
 
 const app = express();
-app.use(cors({ origin: true }));
+app.use(cors({origin: true}));
 
-app.get('/hello-world', (req, res) => {
-  return res.status(200).send('Hello World!');
+app.get("/hello-world", (req, res) => {
+  return res.status(200).send("Hello World!");
 });
 
-exports.app = functions.https.onRequest(app);
+app.post("/api/create", (req, res) => {
+  (async () => {
+    try {
+      functions.logger.info("Create: " + req.body, {structuredData: true});
+      await db
+          .collection("items")
+          .doc("/" + req.body.id + "/")
+          .create({item: req.body.item});
+      return res.status(200).send();
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  })();
+});
 
+// read item
+app.get("/api/read/:item_id", (req, res) => {
+  (async () => {
+    try {
+      const document = db.collection("items").doc(req.params.item_id);
+      const item = await document.get();
+      const response = item.data();
+      return res.status(200).send(response);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  })();
+});
+
+// read all
+app.get("/api/read", (req, res) => {
+  (async () => {
+    try {
+      const query = db.collection("items");
+      const response: any[] = [];
+      await query.get().then((querySnapshot) => {
+        const docs = querySnapshot.docs;
+        for (const doc of docs) {
+          const selectedItem = {
+            id: doc.id,
+            item: doc.data().item,
+          };
+          response.push(selectedItem);
+        }
+      });
+      return res.status(200).send(response);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  })();
+});
+
+// update
+app.put("/api/update/:item_id", (req, res) => {
+  (async () => {
+    try {
+      const document = db.collection("items").doc(req.params.item_id);
+      await document.update({
+        item: req.body.item,
+      });
+      return res.status(200).send();
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  })();
+});
+
+// delete
+app.delete("/api/deconste/:item_id", (req, res) => {
+  (async () => {
+    try {
+      const document = db.collection("items").doc(req.params.item_id);
+      await document.delete();
+      return res.status(200).send();
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  })();
+});
+
+// export const helloWorld = functions.https.onRequest(app);
+exports.app = functions.https.onRequest(app);
